fca0e8bf584eb3d170b64a282fe504898bc7312e
Maysa Horita <maysa.horita@sprint.com>
2018-12-15 20:52:09 -0200

    fixed typo

--- server/server.js
+++ server/server.js
@@ -7,6 +7,6 @@ const app = express();
 
 app.use(express.static(publicPath));
 
-app.listen(app, () => {
-    console.log(`Started on port ${app}`);
+app.listen(port, () => {
+    console.log(`Started on port ${port}`);
 });
