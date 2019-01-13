c9a024ff299033ac865e205093f9f6b12403c857
Maysa Horita <maysa.horita@sprint.com>
2019-01-13 18:55:38 -0200

    Add form for messages and show incoming messages in browser

--- public/js/index.js
+++ public/js/index.js
@@ -6,12 +6,6 @@ socket.on('connect', function () {
     socket.emit('login', {
         user: 'Admin'    
     });
-
-    socket.emit('createMessage', {
-        from: 'me',
-        to: 'jen@example.com',
-        text: 'Hey. This is me'
-    });
 });
 
 socket.on('disconnect', function () {
@@ -20,4 +14,18 @@ socket.on('disconnect', function () {
 
 socket.on('newMessage', function (newMessage) {
     console.log('New email', newMessage);
-});;
+    var li = jQuery('<li></li>');
+    li.text(`${newMessage.from}: ${newMessage.text}`);
+
+    jQuery('#messages').append(li);
+});
+
+jQuery('#message-form').on('submit', function (e) {
+    e.preventDefault();
+    socket.emit('createMessage', {
+        from: 'User',
+        text: jQuery('[name=message]').val()
+    }, function() {
+
+    });
+});
